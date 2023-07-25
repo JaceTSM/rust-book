8c8a767c4bbb5ad9a6ebcf9de9b344835c668285
Tim Murphy <jac3tssm@gmail.com>
2023-07-25 00:36:42 -0400

    minigrep

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let r = Rectange(11, 12);
-    println!("{}: Area={}", r, r.area);
+    // can't do Rectangle(11, 12) it seems :/
+    let r = Rectange { width: 11, height: 12};
+    println!("{:?}: Area={}", r , r.area());
+
+    let p = Point(1.0,2.0,3.0);
+    println!("Point: {:?}", p);
 }
 
-
+#[derive(Debug)]
 struct Rectange {
     width: u32,
     height: u32,
@@ -12,5 +16,30 @@ struct Rectange {
 impl Rectange {
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+}
+
+#[derive(Debug)]
+struct Point(f64, f64, f64);
+
+
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn test_the_first() {
+        println!("First test!");
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_the_second() {
+        println!("Second test!");
+        assert_eq!(2 + 3, 5);
+    }
+
+    #[test]
+    fn test_again() {
+        println!("Three hops this time!");
+        assert_eq!(1 + 2, 3);
     }
 }
